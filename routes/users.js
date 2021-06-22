7e6fb41ed8b654288ae1e282be36857ce21119b2
Juan Mello <juanribeirofx@gmail.com>
2021-06-22 19:08:42 -0300

    Listando usuarios do banco NeDB

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,46 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename:'users.db',
+    autoload:true
+})
+
 module.exports = app => {
 
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(JSON.stringify({
-            users:[{
-                name: 'Juan',
-                email: 'juanribeirofx@gmail.com',
-                id: 1
-            }]
-            
-        }));
+        db.find({}).sort({name:1}).exec((err, users) => {
+
+            if(err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+     
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(users);
+
+            }
+
+        });
     
     });
     
     app.post('/users', (req, res) => {
-    
-        res.json(req.body);
         
+        db.insert(req.body, (err, user) => {
+
+            if(err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+
+        })
     
     });
 
