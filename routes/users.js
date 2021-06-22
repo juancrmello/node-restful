b32bd60fb9cf825eeaa23d2c4cf8c0223a16ce18
Juan Mello <juanribeirofx@gmail.com>
2021-06-22 18:14:08 -0300

    Carregando rotas com consign

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,29 @@
-let express = require('express');
-const { builtinModules } = require('module');
-let routes = express.Router();
+module.exports = app => {
 
-routes.get('/', (req, res) => {
+    app.get('/users', (req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(JSON.stringify({
-        users:[{
-            name: 'Juan',
-            email: 'juanribeirofx@gmail.com',
-            id: 1
-        }]
-        
-    }));
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(JSON.stringify({
+            users:[{
+                name: 'Juan',
+                email: 'juanribeirofx@gmail.com',
+                id: 1
+            }]
+            
+        }));
+    
+    });
+    
+    app.get('/users/admin', (req, res) => {
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(JSON.stringify({
+            users:[]
+            
+        }));
+    
+    });
 
-});
-
-routes.get('/admin', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(JSON.stringify({
-        users:[]
-        
-    }));
-
-});
-
-module.exports = routes;
+}
