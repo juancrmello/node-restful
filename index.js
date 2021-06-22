2d5f9eba0ce96ec747766cf38cbec40d68ff90f4
Juan Mello <juanribeirofx@gmail.com>
2021-06-22 17:29:48 -0300

    Nodemon e criando servidor com express

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
+app.get('/', (req, res) => {
 
-    switch(req.url) {
-
-        case '/':
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Olá</h1>');
-
-        break;
-
-        case '/users':
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            users:[{
-                name: 'Juan',
-                email: 'juanribeirofx@gmail.com',
-                id: 1
-            }]
-            
-        }));
-        break;
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá</h1>');
 
 });
 
-server.listen(3000, 'localhost', () => {
+app.get('/users', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(JSON.stringify({
+        users:[{
+            name: 'Juan',
+            email: 'juanribeirofx@gmail.com',
+            id: 1
+        }]
+        
+    }));
+
+})
+
+app.listen(3000, 'localhost', () => {
 
     console.log('Servidor rodando!');
 
