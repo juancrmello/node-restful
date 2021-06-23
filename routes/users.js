7c846ed7cb09f0661f84be177b11414d663ae202
Juan Mello <juanribeirofx@gmail.com>
2021-06-23 12:39:30 -0300

    Validando dados recebidos de POST e PUT

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,9 @@ module.exports = app => {
     });
     
     route.post((req, res) => {
-        
+
+        if (!app.utils.validator.user(app, req, res)) return false;
+
         db.insert(req.body, (err, user) => {
 
             if(err) {
@@ -57,6 +59,8 @@ module.exports = app => {
     })
  
     routeId.put((req, res) => {
+
+        if (!app.utils.validator.user(app, req, res)) return false;
 
         db.update({_id:req.params.id}, req.body, err => {
 
